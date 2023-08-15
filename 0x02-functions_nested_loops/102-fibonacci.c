4549ff10527f3f685ac7d96daad3c98fcc5d433e
DTAJ095 <albanjaures26@gmail.com>
2023-08-15 16:41:11 -0700

    functions and nested lopps

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 
 /**
- * FibonacciNumbers: check Description
- * Description: it prints the firts fibonacci numbers of 
- * a given number
- */
-void FibonacciNumbers(int n)
-{
-    int N = 0, M = 1, i, next;
-    if (n < 1)
-    {
-        return;
-    }
-    printf("%d"",", N);
-
-    for (i = 1; i < n; i++)
-    {
-        printf("%d"","" ", M);
-        next = N + M;
-        N = M;
-        M = next;
-    }
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * */
-int main()
-{
-    FibonacciNumbers(50);
-    printf("\n");
+ */
 
-    return 0;
+int main(void)
+{
+	int i;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
+	}
+
+	return (0);
 }
