98a84da19fe1c95964d29b0b9ec8802d88ecf7c4
DTAJ095 <albanjaures26@gmail.com>
2023-08-15 11:59:18 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - Check main.h
+ * @n: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
