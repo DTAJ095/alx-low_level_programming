086874e42c3db6651063e309757cb7e647a7f9a9
DTAJ095 <albanjaures26@gmail.com>
2023-08-15 15:57:02 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,11 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n < 0 || n > 15)
+	if (n < 0 && n > 15)
 	{
 		_putchar(' ');
 	}
-	else
+	else if(n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
