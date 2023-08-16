ae9bd99bbc2488e4a29f8673aabc1b728510ead3
DTAJ095 <albanjaures26@gmail.com>
2023-08-15 17:18:51 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
  * main - Entry point
  *
@@ -8,20 +8,30 @@
 
 int main(void)
 {
+	unsigned long int fw1 = 0, bw1 = 1, fw2 = 0, bw2 = 2;
+	unsigned long int half1, half2, half3;
 	int i;
-	long int fibonacci[100];
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
-
-	for (i = 2; i <= 100; i++)
+	printf("%lu, %lu, ", bw1, bw2);
+	for (i = 2; i < 98; i++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 100)
-			printf("%ld\n", fibonacci[i]);
+		if (bw1 + bw2 > LARGEST || fw2 > 0 || fw1 > 0)
+		{
+			half1 = (bw1 + bw2) / LARGEST;
+			half2 = (bw1 + bw2) % LARGEST;
+			half3 = fw1 + fw2 + half1;
+			fw1 = fw2, fw2 = half3;
+			bw1 = bw2, bw2 = half2;
+			printf("%lu%010lu", fw2, bw2);
+		}
 		else
-			printf("%ld, ", fibonacci[i]);
+		{
+			half2 = bw1 + bw2;
+			bw1 = bw2, bw2 = half2;
+			printf("%lu", bw2);
+		}
+		if (i != 97)
+			printf(", ");
 	}
 	printf("\n");
 
