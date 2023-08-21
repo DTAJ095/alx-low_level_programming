e538f85d5c7a0b5ac321fc55cdc86b7a56167f96
DTAJ095 <albanjaures26@gmail.com>
2023-08-21 11:03:06 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,25 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *tmp = dest;
+	/**char *tmp = dest;
 
 	while (*src)
 	{
 		*dest++ = *src++;
 	}
-	return (tmp);
+	return (tmp);*/
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
