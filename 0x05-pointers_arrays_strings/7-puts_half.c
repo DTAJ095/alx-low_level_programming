26c54f1528fcaf430b41d8fc71af618f2f0ee080
DTAJ095 <albanjaures26@gmail.com>
2023-08-21 10:10:54 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,14 +20,18 @@ void puts_half(char *str)
 	if (len % 2 == 0)
 	{
 		n = len / 2;
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	else
 	{
 		n = (len - 1) / 2;
-	}
-	for (i = n; i < len; i++)
-	{
-		_putchar(str[i]);
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
