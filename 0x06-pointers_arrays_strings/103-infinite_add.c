bf7ed632c523f286b1d25998dbe785fb527785b0
DTAJ095 <albanjaures26@gmail.com>
2023-08-25 05:03:39 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,25 +12,54 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len = 0, i,j, carry = 0, sum = 0;
+	int i = 0, j = 0, sum, start, d1, d2, carry = 0;
 
-    for (i = n1[len] - 1; i >= 0; i--)
-    {
-        sum += carry;
-        for (j = n2[len]; j >= 0; j--)
-        {
-            sum += (n1[i] - '0') + (n2[j] - '0') + carry;
-        }
-        carry = sum / 10;
-        r[i + 1] = sum % 10 + '0';
-    }
-
-    if (r[len] <= size_r)
-    {
-        return (r);
-    }
-    else
-    {
-        return (0);
-    }
+	while (*(n1 + i) != '\0')
+	    i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	if (i >= j)
+		start = i;
+	else
+		start = j;
+	if (size_r <= start + 1)
+		return (0);
+	r[start + 1] = '\0';
+	i--;
+    	j--; 
+    	size_r--;
+	d1 = *(n1 + i) - '0';
+    	d2 = *(n2 + j) - '0';
+	while (start >= 0)
+	{
+		sum = d1 + d2 + carry;
+		if (sum >= 10)
+			carry = sum / 10;
+		else
+			carry = 0;
+		if (sum > 0)
+		    *(r + start) = (sum % 10) + '0';
+		else
+			*(r + start) = '0';
+		if (i > 0)
+       	 	{
+			i--;
+            		d1 = *(n1 + i) - '0';
+       		}
+		else
+			d1 = 0;
+		if (j > 0)
+        	{
+			j--;
+            		d2 = *(n2 + j) - '0';
+        	}	
+		else
+			d2 = 0;
+		start--; 
+        	size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
