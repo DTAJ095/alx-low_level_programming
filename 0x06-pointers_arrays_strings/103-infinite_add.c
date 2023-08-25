3e726b79cde5174933b0470f36d77c1177b06e5f
DTAJ095 <albanjaures26@gmail.com>
2023-08-25 06:00:06 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,10 +25,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= start + 1)
 		return (0);
 	r[start + 1] = '\0';
-	i--, j--;
-	d1 = *(n1 + i) - '0';
-	d2 = *(n2 + j) - '0';
-
+	i--, j--, d1 = *(n1 + i) - '0', d2 = *(n2 + j) - '0';
 	while (start >= 0)
 	{
 		sum = d1 + d2 + carry;
@@ -41,28 +38,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			*(r + start) = '0';
 		if (i > 0)
-		{
-			i--;
-			d1 = *(n1 + i) - '0';
-		}
+			i--, d1 = *(n1 + i) - '0';
 		else
-		{
 			d1 = 0;
-		}
 		if (j > 0)
-		{
-			j--;
-			d2 = *(n2 + j) - '0';
-		}
+			j--, d2 = *(n2 + j) - '0';
 		else
-		{
 			d2 = 0;
-		}
-
 		start--;
 		size_r--;
 	}
-
 	if (*(r) == '0')
 		return (r + 1);
 	else
