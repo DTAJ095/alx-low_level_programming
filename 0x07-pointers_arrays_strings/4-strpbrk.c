506e1317c778cc1bb9d3096ffb4d15e0eac985e7
DTAJ095 <albanjaures26@gmail.com>
2023-08-28 15:26:42 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * @s: The string
  * @accept: The accepted string
  *
- * Return: Pointer to the byte in s 
+ * Return: Pointer to the byte in s
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 	int i, j;
 	char *ptr;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] > '\0'; i++)
 	{
 		for (j = 0; accept[j] > '\0'; j++)
 		{
