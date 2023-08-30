d3612aa5caebd60dc865c71dbbb914806ce95b53
DTAJ095 <albanjaures26@gmail.com>
2023-08-30 11:48:40 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	if (*s > '\0')
-	{
-		i = _strlen_recursion(s + 1) + 1;
-	}
-	return (i);
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
