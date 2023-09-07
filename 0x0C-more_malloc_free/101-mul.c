13ca5f470ac4c1c188bf61ff8e65a9453bb0e7b7
DTAJ095 <albanjaures26@gmail.com>
2023-09-07 07:49:09 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,70 +1,162 @@
-#include<stdio.h>
 #include "main.h"
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to convert
- *
- * Return: Nothing
+ * _isdigit - checks for digit
+ * @c : character to check
+ * Return:0 or 1
  */
 
-int _atoi(char *s)
+int _isdigit(char *c)
 {
-	int len, i = 0, FLAG = 0, d = 0, n = 0, digit;
-
-	for (len = 0; s[len] != '\0'; len++)
-		;
-
-	while (i < len && FLAG == 0)
+	while (*c != '\0')
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= 48 && s[i] <= 57)
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			FLAG = 1;
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
-			FLAG = 0;
-		}
-		i++;
+		if (*c < '0' || *c > '9')
+			return (1);
+		c++;
 	}
-	if (FLAG == 0)
-		return (0);
-
-	return (n);
+	return (0);
 }
 
 /**
- * main - Entry point
- * Description: program that multiplies two numbers
- * @argc: argument count
- * @argv: array containing the command lines arguments
+ * _strlen - get the length of strings
+ * @str: string to get length of
  *
- * Return: Always 0 (Success)
- */
+ * Return: length of string
+*/
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: input pointer that represents memory block to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using memset function
+ *
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer to new allocated memory
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
+
+
+/**
+ * multiply - multiplies two arrays with 0 byte
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: nothing
+*/
+
+void multiply(char *s1, char *s2)
+{
+	int i, len1, len2, len, first_digit, second_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	tmp = len2;
+	len = len1 + len2;
+	ptr = _calloc(sizeof(int), len);
+
+	/* store our pointer address to free later */
+	temp = ptr;
+
+	for (len1--; len1 >= 0; len1--)
+	{
+		first_digit = s1[len1] - '0';
+		res = 0;
+		len2 = tmp;
+		for (len2--; len2 >= 0; len2--)
+		{
+			second_digit = s2[len2] - '0';
+			res += ptr[len2 + len1 + 1] + (first_digit * second_digit);
+			ptr[len1 + len2 + 1] = res % 10;
+			res /= 10;
+		}
+		if (res)
+			ptr[len1 + len2 + 1] = res % 10;
+	}
+
+	while (*ptr == 0)
+	{
+		ptr++;
+		len--;
+	}
+
+	for (i = 0; i < len; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+	free(temp);
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 on success 98 on failure
+*/
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	char *num1 = argv[1];
+	char *num2 = argv[2];
 
-	if (argc != 3)
-	{
-		printf("Error\n");
+	if (argc != 3 || _isdigit(num1) || _isdigit(num2))
 		exit(98);
+
+	if (*num1 == '0' || *num2 == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
 	}
 	else
-	{
-		num1 = _atoi(argv[1]);
-		num2 = _atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-	}
-
+		multiply(num1, num2);
 	return (0);
 }
