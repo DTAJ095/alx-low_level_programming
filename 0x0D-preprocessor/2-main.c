6b12d2b11058f0e6f63f6d459dc43982cffb6157
DTAJ095 <albanjaures26@gmail.com>
2023-09-11 06:50:01 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: program that prints the name of the file it was compiled from, followed by a new line.
+ * Description: program that prints the name of the file it was compiled from
  *
  * Return: Always 0 (Success)
  */
