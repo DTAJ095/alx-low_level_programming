ac21a68bafa2a69c3368d87290767af05ff31a9e
DTAJ095 <albanjaures26@gmail.com>
2023-09-14 10:04:37 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,28 +9,34 @@
 
 void print_all(const char * const format, ...)
 {
-	int i, check_status;
+	int i, check_status, j;
 	char *string;
+	const char total_arg[] = "ifcs";
 	va_list vlist;
 
 	va_start(vlist, format);
-
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
+		j = 0;
+		while (total_arg[j])
+		{
+			if (format[i] == total_arg[j] && check_status == 0)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
 		switch (format[i])
 		{
 			case 'i':
-				printf("%d", va_arg(vlist, const int));
-				check_status = 0;
+				printf("%d", va_arg(vlist, const int)), check_status = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(vlist, double));
-				check_status = 0;
+				printf("%f", va_arg(vlist, double)), check_status = 0;
 				break;
 			case 'c':
-				printf("%c", va_arg(vlist, const int));
-				check_status = 0;
+				printf("%c", va_arg(vlist, const int)), check_status = 0;
 				break;
 			case 's':
 				string = va_arg(vlist, char *);
@@ -38,13 +44,7 @@ void print_all(const char * const format, ...)
 					string = "(nil)";
 				printf("%s", string);
 				break;
-			default:
-				check_status = 1;
-				break;
-		}
-		if (format[(i + 1)] != '\0' && check_status == 0)
-			printf(", ");
-		i++;
+		} i++;
 	}
 	printf("\n");
 	va_end(vlist);
