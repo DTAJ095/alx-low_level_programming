243a9923822310043792f3510c9c3db051617d39
DTAJ095 <albanjaures26@gmail.com>
2023-09-21 15:06:04 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: the pointer to the head of the list
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while(head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
