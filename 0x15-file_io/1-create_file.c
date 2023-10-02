104262a5dbb9f16e46beac976066057e9d020f1d
DTAJ095 <albanjaures26@gmail.com>
2023-10-02 06:44:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content)
 		len++;
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -1)
