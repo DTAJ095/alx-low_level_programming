d5fff566aadd1536e4d87985249f476dab1c31ea
DTAJ095 <albanjaures26@gmail.com>
2023-10-02 07:10:10 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_RDWR | O_APPEND);
+	op = open(filename, O_WRONLY | O_APPEND);
 	if (op < 1)
 		return (-1);
 	if (text_content == NULL)
