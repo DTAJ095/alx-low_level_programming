7d9f9128daba2f1061b3ef609d06eb0e065c29b3
DTAJ095 <albanjaures26@gmail.com>
2023-11-09 10:10:29 -0800

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_nd, *tmp= *h;
+	dlistint_t *new_nd, *tmp = *h;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
