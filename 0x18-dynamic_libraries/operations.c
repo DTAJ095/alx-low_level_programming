c10ec5f7f308681a501ad94a302f5d2d7fb07c78
DTAJ095 <albanjaures26@gmail.com>
2023-11-17 01:36:42 -0800

    added

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add - calculates the sum of two numbers
+ * @a: the first number
+ * @b: the second
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: a number
+ * @b: a number
+ *
+ * Return: sub of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: an int
+ * @b: an int
+ *
+ * Return: mul a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: an int
+ * @b: an int
+ *
+ * Return: div a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulo
+ * @a: an int
+ * @b: an int
+ *
+ * Return: mod a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
