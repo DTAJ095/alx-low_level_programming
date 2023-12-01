9a83cc9212a049c871d709cefa588ccbde324a52
DTAJ095 <albanjaures26@gmail.com>
2023-12-01 09:50:04 -0800

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,71 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @key: the key
+ * @value: the value
+ *
+ * Return: 1 if Succeded or 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *h_node, *tmp;
+	char *new_val;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_val = strdup(value);
+			if (new_val == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_val;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	h_node = create_hash_node(key, value);
+	if (h_node == NULL)
+		return (0);
+	h_node->next = ht->array[idx];
+	ht->array[idx] = h_node;
+	return (1);
+}
+
+/**
+ * create_hash_node - create a new hash node
+ * @key: the key
+ * @value: the value of the node
+ *
+ * Return: the node
+ */
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
