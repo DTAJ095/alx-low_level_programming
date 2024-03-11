4499044848f7aedce293173b406a3e5f37b8220e
DTAJ095 <albanjaures26@gmail.com>
2024-03-11 16:00:06 -0700

    added

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,7 +26,7 @@ int binary_search(int *array, size_t size, int value)
 		mid = start + (end - start) / 2;
 		if (array[mid] == value)
 			return (mid);
-		if (array[mid]> value)
+		if (array[mid] > value)
 			end = mid - 1;
 		else
 			start = mid + 1;
