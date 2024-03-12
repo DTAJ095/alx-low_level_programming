c2653a2dd2eea1ad34518b65b9ae398673342a19
DTAJ095 <albanjaures26@gmail.com>
2024-03-12 06:48:09 -0700

    addded

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -18,7 +18,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 
 	step = 0;
 	step_size = sqrt(size);
-	for (node = jump = list; jump ->index + 1 < size && jump->n < value;)
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
 		node = jump;
 		for (step += step_size; jump->index < step; jump = jump->next)
